4ded59b4d93a57286c07e9b27fcf862001f4a03b
prachi <prachi.indurkar007@gmail.com>
2024-12-02 17:37:16 +0530

    fix styles, add nuxtlink

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="footer-bg-color text-white mt-5">
         <div class="flex px-8 py-4 justify-between border-b-2 ">
-            <h2>The Millet Store</h2>
+            <h2>
+                <NuxtLink class="hover:text-pink-400 transition duration-500 " to="/">
+                    The Millet Store
+                </NuxtLink>
+            </h2>
             <div class="flex">
                 <a class="bg-white text-black p-2 rounded me-2" href="mailto:hello@themilletstore.in">
                     <LucideMail />
@@ -22,12 +26,12 @@
                 <h2 class="mb-3">About Us</h2>
                 <ul>
                     <li>
-                        <a href="/about" class= "hover:text-pink-400 transition duration-500 cursor-pointer">
+                        <a href="/about" class="hover:text-pink-400 transition duration-500">
                             Our Story
                         </a>
                     </li>
                     <li>
-                        <a href="/learn" class= "hover:text-pink-400 transition duration-500 cursor-pointer">
+                        <a href="/learn" class="hover:text-pink-400 transition duration-500 ">
                             Learn with Us
                         </a>
                     </li>
@@ -37,13 +41,15 @@
                 <h2 class="mb-3">Help</h2>
                 <ul>
                     <li>
-                        <a href="/terms" class= "hover:text-pink-400 transition duration-500 cursor-pointer">Terms</a>
+                        <a href="/terms" class="hover:text-pink-400 transition duration-500 ">Terms</a>
                     </li>
                     <li>
-                        <a href="/privacy" class= "hover:text-pink-400 transition duration-500 cursor-pointer">Privacy</a>
+                        <a href="/privacy"
+                            class="hover:text-pink-400 transition duration-500 ">Privacy</a>
                     </li>
                     <li>
-                        <a href="/shipping" class= "hover:text-pink-400 transition duration-500 cursor-pointer">Shipping & Returns</a>
+                        <a href="/shipping" class="hover:text-pink-400 transition duration-500 ">Shipping
+                            & Returns</a>
                     </li>
                 </ul>
             </div>
