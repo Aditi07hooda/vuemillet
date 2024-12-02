9ec1f64c8a8a3320782f5159874173bad349fb57
prachi <prachi.indurkar007@gmail.com>
2024-12-02 17:10:02 +0530

    update privacy page

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,1 +1,22 @@
-<template>Privacy page</template>
+<template>
+    <div class="mx-auto p-14">
+        <h1 class="mb-4 text-center"><strong>Privacy Policy</strong></h1>
+        <p class='px-8 py-4'>Last updated: November 30, 2021</p>
+        <p class='px-8 py-4'>We Collect your name, email, phone number and address for the purpose of managing your
+            orders</p>
+        <p class='px-8 py-4'>We are in the business of making your food delicious, not in trading your data</p>
+        <p class='px-8 py-4'>We do not sell/share your personal information with any other person/company</p>
+        <p class='px-8 py-4'>You can choose to close your account with us and we will delete all information.</p>
+        <p class='px-8 py-4'>Please Contact Us at hello@themilletstore.in, we will be happy to answer any questions that
+            you might have
+        </p>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    text-transform: capitalize;
+    font-weight: 900;
+    font-size: 35px;
+}
+</style>
