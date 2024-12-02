4f00c1ae8c2dcd2c0cea9511f9077109809d4e44
prachi <prachi.indurkar007@gmail.com>
2024-12-02 12:18:56 +0530

    configure env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  // Add runtimeConfig
+  runtimeConfig: {
+    // Server-side only variables
+    baseURL: process.env.BASE_URL,
+    brandID: process.env.BRAND_ID,
+
+    // Public variables (accessible client-side)
+    public: {
+      baseURL: process.env.BASE_URL, // Include only if required on client-side
+      brandID: process.env.BRAND_ID,
+    },
+  },
 })
