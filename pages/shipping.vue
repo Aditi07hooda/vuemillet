009602fb7577347c345c02bd281e48a65537817a
prachi <prachi.indurkar007@gmail.com>
2024-12-02 17:17:02 +0530

    design privacy page

--- pages/shipping.vue
+++ pages/shipping.vue
@@ -1,1 +1,25 @@
-<template>Shipping page</template>
+<template>
+    <div class="mx-auto p-14">
+        <h1 class="mb-4 text-center"><strong>Shipping &amp; Returns</strong></h1>
+        <p class='px-8 py-4'>Last updated: <span class="text-blue-500 underline">November 30, 2021</span></p>
+        <p class='px-8 py-4'>We will try to deliver your order within 2 business days.</p>
+        <p class='px-8 py-4'>Incase of any delay due to any unforeseeable issue, we will cancel your order and refund if
+            you wish so.</p>
+        <p class='px-8 py-4'>We charge a shipping charge if order value is less than a particular amount.</p>
+        <p class='px-8 py-4'>In case you are not happy with the products we will be happy to refund or replace the
+            products.</p>
+        <p class='px-8 py-4'>Please Contact Us at
+            <a href="mailto:hello@themilletstore.in" class="text-blue-500 hover:underline">hello@themilletstore.in</a>,
+            we will be happy to answer any
+            questions that you might have.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    text-transform: capitalize;
+    font-weight: 900;
+    font-size: 35px;
+}
+</style>
