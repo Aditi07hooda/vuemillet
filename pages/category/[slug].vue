822ff2febb1d305fb436d864849081096a41cf33
prachi <prachi.indurkar007@gmail.com>
2024-12-13 17:01:20 +0530

    add hero image and fix its css

--- pages/category/[slug].vue
+++ pages/category/[slug].vue
@@ -28,9 +28,13 @@ console.log('product in each categoru p;a', products.value)
 </script>
 <template>
     <div>
-        <h1>{{ products[0]?.name }}</h1>
-        <img :src="products[0]?.imageUrl" alt="h" />
-        <div v-html="products[0]?.description"></div>
+        <div class="hero-image px-14 mt-7">
+            <img :src="products[0]?.imageUrl" alt="hey" />
+            <div class="hero-overlay">
+                <h1 class="text-center uppercase">{{ products[0]?.name }}</h1>
+            </div>
+        </div>
+        <div class="p-14 description" v-html="products[0]?.description"></div>
         <div v-if="products.length !== 0" class="flex justify-evenly w-full flex-wrap">
             <div v-for="product in products[0].products" :key="product.id"
                 class="product-container rounded mb-10 mx-2 w-full sm:w-auto">
@@ -38,9 +42,65 @@ console.log('product in each categoru p;a', products.value)
             </div>
         </div>
     </div>
-    This is the each category's product page
 </template>
-<style>
+
+<style scoped>
+.hero-image {
+    position: relative;
+    width: 100%;
+    height: 50vh;
+    overflow: hidden;
+}
+
+.hero-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.hero-overlay {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 20px;
+    border-radius: 10px;
+}
+
+.hero-overlay h1 {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+}
+
+.hero-overlay p {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+}
+
+
+@media (max-width: 768px) {
+    .hero-overlay h1 {
+        font-size: 2rem;
+    }
+
+    .hero-overlay p {
+        font-size: 1rem;
+    }
+
+    .hero-overlay button {
+        font-size: 0.9rem;
+        padding: 8px 16px;
+    }
+}
+
+.description{
+    font-size: 20px;
+    font-family: "Futura PT";
+}
+
 .product-container {
     flex: 1 1 calc(25% - 10px);
     max-width: 250px;
