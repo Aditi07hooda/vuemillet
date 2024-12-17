6275c2233bdb1069b337e6a0abc999444fb02326
prachi <prachi.indurkar007@gmail.com>
2024-12-17 11:47:52 +0530

    fetch details of each product in its slug page

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -1,4 +1,11 @@
 <script setup>
+const config = useRuntimeConfig()
+const baseURL = config.public.baseURL
+const brandID = config.public.brandID
+const route = useRoute()
+const { data: product, error, loading } = await useFetch(`${baseURL}/store/${brandID}/products/${route.params.slug}`)
+console.log("data of each product", product.value, route.params)
+
 </script>
 <template>
     This is each products page
