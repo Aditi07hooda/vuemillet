6b1f33387919d41e2889d9816486008c06605f4b
prachi <prachi.indurkar007@gmail.com>
2024-12-20 15:59:41 +0530

    create cart component

---
+++ components/Cart.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="cart-container">
+        This is cart component
+        Lorem, ipsum dolor sit 
+    </div>
+</template>
+
+<style scoped>
+.cart-container {
+    z-index: 999;
+}
+</style>
