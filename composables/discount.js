8ca4fc94b672342b36e7f48a7ecfeab21fce9610
prachi <prachi.indurkar007@gmail.com>
2024-12-24 12:22:15 +0530

    create separate component to show discount, dont show discount if offerprice and price are equal

--- composables/discount.js
+++ composables/discount.js
@@ -2,3 +2,7 @@ export const calculateDiscount = (price, offerPrice) => {
     const discount = ((price - offerPrice) / price) * 100
     return Math.round(discount)
 }
+
+export const hasDiscount = (price, offerPrice) => {
+    return calculateDiscount(price, offerPrice) !== 0
+}
