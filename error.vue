607e65e4ceeabbc4ba812c064edfa8bdd233b675
Aditi <aditi07hooda@gmail.com>
2025-01-03 12:27:07 +0530

    making custom 404 page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { clearError } from '#app'
+
+const props = defineProps({
+  error: Object
+})
+
+const handleError = () => {
+  clearError({ redirect: '/' })
+}
+</script>
+
+<template>
+  <div>
+    <NotFound />
+    <button @click="handleError">Clear errors</button>
+  </div>
+</template>
