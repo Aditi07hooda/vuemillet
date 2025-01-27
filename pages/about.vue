864055e3da7307807dcf63c193a2af09d331a5d6
Aditi <aditi07hooda@gmail.com>
2025-01-27 17:10:51 +0530

    code format

--- pages/about.vue
+++ pages/about.vue
@@ -1,106 +1,106 @@
 <template>
-    <div class="mx-auto md:p-14 overflow-x-hidden p-4">
-      <h1 class="text-center mb-4">The Story of Millet Store</h1>
-      <p class="md:px-8 py-4">
-        Every story has a beginning. Ours began one fine morning with a simple and
-        innocent question posed by my little daughter, "Why does chapatti have no
-        taste, amma?" Taken aback but intrigued, I undertook a year-long journey
-        to understand the food we eat - traditional and modern; and ways to make
-        them even better.
-      </p>
-      <p class="md:px-8 py-4">
-        After many trials and errors and much experimentation on my family, the
-        perfect atta recipe was finally found. The result was a wide variety of
-        flours that made the everyday rotis different and interesting. Our atta
-        dough is easy to handle and makes amazing rotis that are great in taste
-        and stay soft for up to 12 hours.
-      </p>
-      <p class="md:px-8 py-4">
-        Encouraged by the overwhelming response received from friends and family,
-        I decided to open 'The Millet Store' in September 2019. Since then, we
-        have introduced freshly milled flours (rice, ragi, jowar, buckwheat, etc),
-        wood pressed oil (coconut, groundnut and sesame) and jaggery from our own
-        farm.
-      </p>
-      <p class="md:px-8 py-4">
-        We have also developed our signature attas (Sharbati, Emmer, Fenugreek
-        &amp; Poppy Seeds), Multi-Grain (8 grains), Multi-Millet (5 millets),
-        Methi Masala Atta, Drumstick Leaf Masala Atta and many more.
-      </p>
-      <p class="md:px-8 py-4">
-        The Millet Store specialises in making ready mixes and special attas,
-        which are easy to use and store. The ingredients are freshly sourced and
-        each product is customised to suit the taste and varying requirements of
-        our customers. If you want it, we can make it!
-      </p>
-      <p class="md:px-8 py-4">
-        We work diligently to offer you the best in taste, nutrition and
-        convenience. The Millet Store maintains strict hygiene and quality
-        standards to retain the freshness and nutritional content of every single
-        ingredient that we use.
-      </p>
-      <p class="md:px-8 py-4">
-        Through our products, we hope to re-introduce the wonders and benefits of
-        whole grains and flours and make them equally tempting for all age groups.
-        We understand the nutritional needs of children, adults and seniors; and
-        the practical constraints of a busy schedule, along with the challenges it
-        throws up for healthy home-cooked meals.
-      </p>
-      <p class="md:px-8 py-4">
-        Our healthy ready mixes and flours lend a helping hand to any home cook
-        who needs it - students, young working adults, fresh cooks or anyone
-        looking to cook up a delicious storm in the kitchen super quick. No
-        preparation, no mess, no cutting, no chopping, no cleaning and no fuss. We
-        assure our customers of fast and easy products, which they can use
-        anytime.
-      </p>
-      <p class="md:px-8 py-4">
-        The moment you place an order, we get to work procuring fresh organic
-        produce, milling them and packing them under stringent safety measures. We
-        deliver within the time frame we promise. Since we do not add any kind of
-        preservatives, our products have a limited shelf life and must be consumed
-        within the specified date.
-      </p>
-      <p class="md:px-8 py-4">
-        Our promise is simple - We strive to make food delicious for you and your
-        family.
-      </p>
-      <p class="md:px-8 py-4">And our story has only just begun!</p>
-      <p class="md:px-8 py-4">
-        We deliver freshly made atta, flours, masala and oil to your doorstep,
-        once a week. Just Call/WhatsApp us on
-        <a
-          href="https://wa.me/916362033034"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-green-500 underline"
-          >+91 63620 33034</a
-        >
-        for any queries. We would love to hear from you. Your valuable suggestions
-        help us serve you better.
-      </p>
-      <p class="md:px-8 py-4">
-        Thanks <br />
-        <em>Ponmozhi &amp; Team</em>
-      </p>
-      <div class="flex justify-center md:px-8 md:py-4">
-        <div class="relative w-full" style="padding-top: 56.25%">
-          <iframe
-            class="absolute top-0 left-0 w-full h-full"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d794.3228473378243!2d77.56305214446898!3d12.896481872398201!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3f16ea409363%3A0xe7bff0e639fd7616!2sThe%20Millet%20Store!5e0!3m2!1sen!2sin!4v1643796451687!5m2!1sen!2sin"
-            style="border: 0"
-            allow="fullscreen"
-            loading="lazy"
-          ></iframe>
-        </div>
+  <div class="mx-auto md:p-14 overflow-x-hidden p-4">
+    <h1 class="text-center mb-4">The Story of Millet Store</h1>
+    <p class="md:px-8 py-4">
+      Every story has a beginning. Ours began one fine morning with a simple and
+      innocent question posed by my little daughter, "Why does chapatti have no
+      taste, amma?" Taken aback but intrigued, I undertook a year-long journey
+      to understand the food we eat - traditional and modern; and ways to make
+      them even better.
+    </p>
+    <p class="md:px-8 py-4">
+      After many trials and errors and much experimentation on my family, the
+      perfect atta recipe was finally found. The result was a wide variety of
+      flours that made the everyday rotis different and interesting. Our atta
+      dough is easy to handle and makes amazing rotis that are great in taste
+      and stay soft for up to 12 hours.
+    </p>
+    <p class="md:px-8 py-4">
+      Encouraged by the overwhelming response received from friends and family,
+      I decided to open 'The Millet Store' in September 2019. Since then, we
+      have introduced freshly milled flours (rice, ragi, jowar, buckwheat, etc),
+      wood pressed oil (coconut, groundnut and sesame) and jaggery from our own
+      farm.
+    </p>
+    <p class="md:px-8 py-4">
+      We have also developed our signature attas (Sharbati, Emmer, Fenugreek
+      &amp; Poppy Seeds), Multi-Grain (8 grains), Multi-Millet (5 millets),
+      Methi Masala Atta, Drumstick Leaf Masala Atta and many more.
+    </p>
+    <p class="md:px-8 py-4">
+      The Millet Store specialises in making ready mixes and special attas,
+      which are easy to use and store. The ingredients are freshly sourced and
+      each product is customised to suit the taste and varying requirements of
+      our customers. If you want it, we can make it!
+    </p>
+    <p class="md:px-8 py-4">
+      We work diligently to offer you the best in taste, nutrition and
+      convenience. The Millet Store maintains strict hygiene and quality
+      standards to retain the freshness and nutritional content of every single
+      ingredient that we use.
+    </p>
+    <p class="md:px-8 py-4">
+      Through our products, we hope to re-introduce the wonders and benefits of
+      whole grains and flours and make them equally tempting for all age groups.
+      We understand the nutritional needs of children, adults and seniors; and
+      the practical constraints of a busy schedule, along with the challenges it
+      throws up for healthy home-cooked meals.
+    </p>
+    <p class="md:px-8 py-4">
+      Our healthy ready mixes and flours lend a helping hand to any home cook
+      who needs it - students, young working adults, fresh cooks or anyone
+      looking to cook up a delicious storm in the kitchen super quick. No
+      preparation, no mess, no cutting, no chopping, no cleaning and no fuss. We
+      assure our customers of fast and easy products, which they can use
+      anytime.
+    </p>
+    <p class="md:px-8 py-4">
+      The moment you place an order, we get to work procuring fresh organic
+      produce, milling them and packing them under stringent safety measures. We
+      deliver within the time frame we promise. Since we do not add any kind of
+      preservatives, our products have a limited shelf life and must be consumed
+      within the specified date.
+    </p>
+    <p class="md:px-8 py-4">
+      Our promise is simple - We strive to make food delicious for you and your
+      family.
+    </p>
+    <p class="md:px-8 py-4">And our story has only just begun!</p>
+    <p class="md:px-8 py-4">
+      We deliver freshly made atta, flours, masala and oil to your doorstep,
+      once a week. Just Call/WhatsApp us on
+      <a
+        href="https://wa.me/916362033034"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-green-500 underline"
+        >+91 63620 33034</a
+      >
+      for any queries. We would love to hear from you. Your valuable suggestions
+      help us serve you better.
+    </p>
+    <p class="md:px-8 py-4">
+      Thanks <br />
+      <em>Ponmozhi &amp; Team</em>
+    </p>
+    <div class="flex justify-center md:px-8 md:py-4">
+      <div class="relative w-full" style="padding-top: 56.25%">
+        <iframe
+          class="absolute top-0 left-0 w-full h-full"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d794.3228473378243!2d77.56305214446898!3d12.896481872398201!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3f16ea409363%3A0xe7bff0e639fd7616!2sThe%20Millet%20Store!5e0!3m2!1sen!2sin!4v1643796451687!5m2!1sen!2sin"
+          style="border: 0"
+          allow="fullscreen"
+          loading="lazy"
+        ></iframe>
       </div>
     </div>
-  </template>
-  
-  <style scoped>
-  h1 {
-    text-transform: uppercase;
-    font-weight: 900;
-    font-size: 35px;
-  }
-  </style>  
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-transform: uppercase;
+  font-weight: 900;
+  font-size: 35px;
+}
+</style>
