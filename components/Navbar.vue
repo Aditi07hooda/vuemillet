d74cbd4b50cf5a8e0eea19a6deba4f69e9b5164a
Aditi <aditi07hooda@gmail.com>
2025-01-28 11:08:56 +0530

    collection list in search

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { useCartModelVisibilty } from "~/store/cart"
+import { useCartModelVisibilty } from "~/store/cart";
 
-const cartModelVisible = useCartModelVisibilty()
-const isOpen = ref(false)
-const router = useRouter()
-const isSearchOpen = ref(false)
+const cartModelVisible = useCartModelVisibilty();
+const isOpen = ref(false);
+const router = useRouter();
+const isSearchOpen = ref(false);
 const closeModal = () => {
   isSearchOpen.value = false;
-}
+};
 
 const menuItems = [
   {
@@ -19,19 +19,19 @@ const menuItems = [
     label: "Learn",
     slot: "learn",
   },
-]
+];
 
-const config = useRuntimeConfig()
-const baseURL = config.public.baseURL
-const brandID = config.public.brandID
-const sessionId = ref(null)
+const config = useRuntimeConfig();
+const baseURL = config.public.baseURL;
+const brandID = config.public.brandID;
+const sessionId = ref(null);
 
 if (typeof window !== "undefined") {
-  sessionId.value = localStorage.getItem("sessionId")
+  sessionId.value = localStorage.getItem("sessionId");
 }
 
 if (!sessionId.value) {
-  sessionId.value = await createSessionId(baseURL, brandID)
+  sessionId.value = await createSessionId(baseURL, brandID);
 }
 
 const {
@@ -42,41 +42,66 @@ const {
   headers: {
     session: sessionId.value,
   },
-})
+});
+
+const {
+  data: collection,
+  error: collectionError,
+  loading: collectionLoading,
+} = useFetch(`${baseURL}/store/${brandID}/collections`, {
+  headers: {
+    session: sessionId.value,
+  },
+});
 
 const accountNavigation = () => {
   if (localStorage.getItem("user")) {
-    router.push("/account")
+    router.push("/account");
   } else {
-    router.push("/account/login")
+    router.push("/account/login");
   }
-}
-const handleClose=()=>{
-  isOpen.value=false
+};
+const handleClose = () => {
+  isOpen.value = false;
   // nextTick(()=>{
   //   $router.push(r)
   // })
-}
+};
 </script>
 
 <template>
   <div
-    class="flex md:justify-evenly justify-between bg-inherit w-full md:pt-4 md:px-24 p-3 items-center overflow-x-hidden">
+    class="flex md:justify-evenly justify-between bg-inherit w-full md:pt-4 md:px-24 p-3 items-center overflow-x-hidden"
+  >
     <div class="flex justify-between items-center md:w-full">
       <UPopover overlay :popper="{ placement: 'bottom-start' }" class="">
-        <UButton label="Shop" trailing-icon="i-heroicons-chevron-down-20-solid"
-          class="uppercase font-semibold hidden bg-inherit text-black items-center md:flex text-md hover:bg-inherit" />
+        <UButton
+          label="Shop"
+          trailing-icon="i-heroicons-chevron-down-20-solid"
+          class="uppercase font-semibold hidden bg-inherit text-black items-center md:flex text-md hover:bg-inherit"
+        />
         <template #panel="{ close }">
-          <div class="grid grid-flow-row grid-cols-5 m-5 w-full mb-36 mx-20 mt-8 gap-24">
+          <div
+            class="grid grid-flow-row grid-cols-5 m-5 w-full mb-36 mx-20 mt-8 gap-24"
+          >
             <div>
-              <h3 class="uppercase text-lg font-semibold py-3">By Categories</h3>
+              <h3 class="uppercase text-lg font-semibold py-3">
+                By Categories
+              </h3>
               <div class="flex flex-col">
                 <ul class="flex flex-col space-y-4">
-                  <li v-for="category in categories" class="flex gap-3 text-sm font-medium items-center">
+                  <li
+                    v-for="category in categories"
+                    class="flex gap-3 text-sm font-medium items-center"
+                  >
                     <LucideChevronRight class="w-4 h-4" />
-                    <NuxtLink :to="`/category/${category.slug}`" @click="close"
-                      class="w-full hover:text-pink-600 hover:scale-105 transition duration-500">
-                      {{ capitalize(category.name) }} </NuxtLink>
+                    <NuxtLink
+                      :to="`/category/${category.slug}`"
+                      @click="close"
+                      class="w-full hover:text-pink-600 hover:scale-105 transition duration-500"
+                    >
+                      {{ capitalize(category.name) }}
+                    </NuxtLink>
                   </li>
                 </ul>
               </div>
@@ -90,8 +115,11 @@ const handleClose=()=>{
       <div class="uppercase font-semibold hidden md:block">
         <NuxtLink to="/about">About Us</NuxtLink>
       </div>
-      <UButton @click="isOpen = true" label="open"
-        class="md:hidden bg-inherit text-black w-fit p-0 m-0 hover:bg-inherit">
+      <UButton
+        @click="isOpen = true"
+        label="open"
+        class="md:hidden bg-inherit text-black w-fit p-0 m-0 hover:bg-inherit"
+      >
         <div class="text-black md:hidden">
           <LucideMenu />
         </div>
@@ -100,34 +128,59 @@ const handleClose=()=>{
         <LucideSearch @click="isSearchOpen = true" />
       </div>
     </div>
-    <div class="flex justify-center uppercase w-full font-bold md:text-2xl text-xl">
+    <div
+      class="flex justify-center uppercase w-full font-bold md:text-2xl text-xl"
+    >
       <ULink to="/">The Millet Store </ULink>
     </div>
-    <div class="flex justify-between gap-3 md:w-full ">
-      <span label="Search" trailing-icon="i-heroicons-chevron-down-20-solid"
+    <div class="flex justify-between gap-3 md:w-full">
+      <span
+        label="Search"
+        trailing-icon="i-heroicons-chevron-down-20-solid"
         class="uppercase font-semibold hidden bg-inherit items-center md:flex text-md hover:bg-inherit cursor-pointer"
-        @click="isSearchOpen = true">
-        Search</span>
+        @click="isSearchOpen = true"
+      >
+        Search</span
+      >
       <div class="bg-red-600">
         <UModal v-model="isSearchOpen" fullscreen prevent-close>
           <div class="flex justify-end px-14 m-5">
-            <UButton color="gray" variant="ghost" class="-my-1" @click="isSearchOpen = false">
+            <UButton
+              color="gray"
+              variant="ghost"
+              class="-my-1"
+              @click="isSearchOpen = false"
+            >
               Go Back
             </UButton>
           </div>
-          <SearchPopover :categories="categories" :closeModal="closeModal" />
+          <SearchPopover
+            :categories="categories"
+            :closeModal="closeModal"
+            :collections="collection"
+          />
         </UModal>
       </div>
-      <div class="uppercase font-semibold hidden md:block cursor-pointer" @click="accountNavigation">
+      <div
+        class="uppercase font-semibold hidden md:block cursor-pointer"
+        @click="accountNavigation"
+      >
         Account
       </div>
       <div class="hidden md:block">
-        <span class="uppercase font-semibold cursor-pointer" @click="cartModelVisible.openCartModel">Cart</span>
+        <span
+          class="uppercase font-semibold cursor-pointer"
+          @click="cartModelVisible.openCartModel"
+          >Cart</span
+        >
         <UModal v-model="cartModelVisible.isCartModelOpen" prevent-close>
           <Cart />
         </UModal>
       </div>
-      <div class="text-black md:hidden cursor-pointer" @click="accountNavigation">
+      <div
+        class="text-black md:hidden cursor-pointer"
+        @click="accountNavigation"
+      >
         <LucideCircleUserRound />
       </div>
       <div class="text-black md:hidden">
@@ -138,23 +191,41 @@ const handleClose=()=>{
   <div class="flex justify-between align-middle items-center">
     <UModal v-model="isOpen" :transition="true" class="items-center" fullscreen>
       <div class="u-modal-content hide-scrollbar">
-        <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid"
-          class="-my-1 text-3xl font-bold p-5 w-12 h-12" @click="isOpen = false" />
+        <UButton
+          color="gray"
+          variant="ghost"
+          icon="i-heroicons-x-mark-20-solid"
+          class="-my-1 text-3xl font-bold p-5 w-12 h-12"
+          @click="isOpen = false"
+        />
         <UAccordion :items="menuItems" variant="soft" color="white" class="p-8">
           <template #learn>
             <div class="flex flex-col space-y-4 pl-8">
-              <NuxtLink to="/blogs" class=" hover:text-pink-600 hover:scale-105 transition duration-500" @click="handleClose">
+              <NuxtLink
+                to="/blogs"
+                class="hover:text-pink-600 hover:scale-105 transition duration-500"
+                @click="handleClose"
+              >
                 Blogs
               </NuxtLink>
-              <NuxtLink to="/about" class=" hover:text-pink-600 hover:scale-105 transition duration-500" @click="handleClose">
+              <NuxtLink
+                to="/about"
+                class="hover:text-pink-600 hover:scale-105 transition duration-500"
+                @click="handleClose"
+              >
                 About Us
               </NuxtLink>
             </div>
           </template>
           <template #categories>
             <div class="flex flex-col space-y-4 pl-8">
-              <NuxtLink v-for="category in categories" :to="`/category/${category.slug}`" :key="category.slug"
-                @click="handleClose" class=" hover:text-pink-600 hover:scale-105 transition duration-500">
+              <NuxtLink
+                v-for="category in categories"
+                :to="`/category/${category.slug}`"
+                :key="category.slug"
+                @click="handleClose"
+                class="hover:text-pink-600 hover:scale-105 transition duration-500"
+              >
                 {{ capitalize(category.name) }}
               </NuxtLink>
             </div>
--- components/SearchPopover.vue
+++ components/SearchPopover.vue
@@ -1,153 +1,190 @@
 <script setup>
-
 defineProps({
-    categories: {
-        type: Object,
-        required: true
-    },
-    closeModal: {
-        type: Function,
-        required: true,
-    },
-})
+  categories: {
+    type: Object,
+    required: true,
+  },
+  closeModal: {
+    type: Function,
+    required: true,
+  },
+  collections: {
+    type: Object,
+    required: true,
+  },
+});
 
-const config = useRuntimeConfig()
-const baseURL = config.public.baseURL
-const brandID = config.public.brandID
-const sessionId = ref(null)
-const searchResults = ref('')
-const route = useRoute().path.split('/')
-const searchQuery = ref(route[1] === 'search' ? route[2] : '')
+const config = useRuntimeConfig();
+const baseURL = config.public.baseURL;
+const brandID = config.public.brandID;
+const sessionId = ref(null);
+const searchResults = ref("");
+const route = useRoute().path.split("/");
+const searchQuery = ref(route[1] === "search" ? route[2] : "");
 
 if (typeof window !== "undefined") {
-    sessionId.value = localStorage.getItem("sessionId")
+  sessionId.value = localStorage.getItem("sessionId");
 }
 
 if (!sessionId.value) {
-    sessionId.value = await createSessionId(baseURL, brandID)
+  sessionId.value = await createSessionId(baseURL, brandID);
 }
 
 const fetchResultsForEachQueryWord = async (query) => {
-    try {
-        const response = await fetch(`${baseURL}/store/${brandID}/search?q=${query}`)
-        let r = await response.json()
-        return r.results || []
-    } catch (e) {
-        console.error(`Error fetching results for query "${query}":`, e)
-        return []
-    }
-}
+  try {
+    const response = await fetch(
+      `${baseURL}/store/${brandID}/search?q=${query}`
+    );
+    let r = await response.json();
+    return r.results || [];
+  } catch (e) {
+    console.error(`Error fetching results for query "${query}":`, e);
+    return [];
+  }
+};
 
 const fetchResultsForQuerySentence = async () => {
-    try {
-        const queries = searchQuery.value.split(' ') 
-        let allResults = []
+  try {
+    const queries = searchQuery.value.split(" ");
+    let allResults = [];
 
-        for (const query of queries) {
-            const results = await fetchResultsForEachQueryWord(query)
-            allResults = allResults.concat(results)
-            console.log('all results',allResults)
-        }
-
-        // Remove duplicates by mapping unique IDs because there are multiple results for some products like pasta and noodles
-        const uniqueResults = Array.from(
-            new Map(allResults.map(item => [item.id, item])).values()
-        )
-
-        searchResults.value = {
-            term: searchQuery.value,
-            results: uniqueResults
-        }
-    } catch (e) {
-        console.error("Error in fetching search results:", e)
+    for (const query of queries) {
+      const results = await fetchResultsForEachQueryWord(query);
+      allResults = allResults.concat(results);
+      console.log("all results", allResults);
     }
-}
+
+    // Remove duplicates by mapping unique IDs because there are multiple results for some products like pasta and noodles
+    const uniqueResults = Array.from(
+      new Map(allResults.map((item) => [item.id, item])).values()
+    );
+
+    searchResults.value = {
+      term: searchQuery.value,
+      results: uniqueResults,
+    };
+  } catch (e) {
+    console.error("Error in fetching search results:", e);
+  }
+};
 
 if (searchQuery.value) {
-    fetchResultsForQuerySentence()
+  fetchResultsForQuerySentence();
 }
-
 </script>
 
 <template>
-    <div class="u-modal-content hide-scrollbar ">
-        <h1 class="text-center text-lg font-semibold pt-3 text-gray-600">Search by name</h1>
-        <div class="px-14 m-5 mt-2 flex w-full">
-            <form class="relative w-full" @submit.prevent="fetchResultsForQuerySentence">
-                <UInput v-model="searchQuery" :ui="{
-                    strategy: 'override',
-                    color: 'bg-gray-600',
-                }" variant="outline" color="green" class="w-full pr-12" placeholder="Search products by name and click enter..." />
-            </form>
-        </div>
-        <div class="flex flex-wrap md:flex-nowrap px-14 w-full mb-36 mt-8 mx-5 gap-6 sm:gap-4 text-gray-600">
-            <div class="md:w-1/4 w-full">
-                <div class="flex flex-col">
-                    <h3 class="text-lg font-semibold py-3">All Categories</h3>
-                    <ul class="flex flex-col space-y-4">
-                        <li v-for="category in categories" :key="category.id"
-                            class="flex gap-3 text-sm font-medium items-center hover:text-pink-600 hover:scale-105 transition duration-500">
-                            <NuxtLink :to="`/category/${category.slug}`" @click='closeModal'>
-                                {{ capitalize(category.name) }}
-                            </NuxtLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div v-if="!searchResults" class="md:w-3/4 w-full">
-                <h3 class="text-lg font-semibold py-3">All Categories</h3>
-                <div class="flex gap-4 flex-wrap">
-                    <div v-for="category in categories" :key="category.id" class="">
-                        <NuxtLink :to="`/category/${category.slug}`" @click='closeModal'
-                            class="flex flex-col items-center hover:text-pink-600 hover:scale-105 transition duration-500">
-                            <img :src="category.imageUrl" :alt="category.name"
-                                class="w-[150px] h-[150px] object-cover rounded-lg" />
-                            <div class="w-[150px] mt-2 font-semibold">{{ category.name }}</div>
-                        </NuxtLink>
-                    </div>
-                </div>
-            </div>
-            <div v-else class="md:w-3/4 w-full">
-                <h3 class="text-lg font-semibold py-3">
-                    <template v-if="searchResults.term">
-                        <template v-if="searchResults.results.length !== 0"> Showing all search results for <span
-                                class="text-red-600">{{
-                                    searchResults.term }}</span>
-                        </template>
-                        <template v-else>No results found for <span class="text-red-600">{{ searchResults.term }}</span>
-                        </template>
-                    </template>
-                    <template v-else>
-                        Recommended products
-                    </template>
-                </h3>
-                <div class="flex flex-wrap gap-4">
-                    <div v-for="product in searchResults.results" :key="product.id">
-                        <NuxtLink :to="`/product/${product.id}`" @click='closeModal'
-                            class="flex flex-col items-center hover:text-pink-600 hover:scale-105 transition duration-500">
-                            <img :src="product.oneImg || product.images[0] || '/favicon.ico'" :alt="product.name"
-                                class="w-[150px] h-[150px] object-cover rounded-lg" />
-                            <div class="w-[150px] mt-2 font-semibold"> {{ capitalize(product.name) }} </div>
-                        </NuxtLink>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="u-modal-content hide-scrollbar">
+    <h1 class="text-center text-lg font-semibold pt-3 text-gray-600">
+      Search by name
+    </h1>
+    <div class="px-14 m-5 mt-2 flex w-full">
+      <form
+        class="relative w-full"
+        @submit.prevent="fetchResultsForQuerySentence"
+      >
+        <UInput
+          v-model="searchQuery"
+          :ui="{
+            strategy: 'override',
+            color: 'bg-gray-600',
+          }"
+          variant="outline"
+          color="green"
+          class="w-full pr-12"
+          placeholder="Search products by name and click enter..."
+        />
+      </form>
     </div>
+    <div
+      class="flex flex-wrap md:flex-nowrap px-14 w-full mb-36 mt-8 mx-5 gap-6 sm:gap-4 text-gray-600"
+    >
+      <div class="md:w-1/4 w-full">
+        <div class="flex flex-col">
+          <h3 class="text-lg font-semibold py-3">All Categories</h3>
+          <ul class="flex flex-col space-y-4">
+            <li
+              v-for="category in collections"
+              :key="category.id"
+              class="flex gap-3 text-sm font-medium items-center hover:text-pink-600 hover:scale-105 transition duration-500"
+            >
+              <NuxtLink :to="`/collections/${category.id}`" @click="closeModal">
+                {{ capitalize(category.name) }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div v-if="!searchResults" class="md:w-3/4 w-full">
+        <h3 class="text-lg font-semibold py-3">All Categories</h3>
+        <div class="flex gap-4 flex-wrap">
+          <div v-for="category in categories" :key="category.id" class="">
+            <NuxtLink
+              :to="`/category/${category.slug}`"
+              @click="closeModal"
+              class="flex flex-col items-center hover:text-pink-600 hover:scale-105 transition duration-500"
+            >
+              <img
+                :src="category.imageUrl"
+                :alt="category.name"
+                class="w-[150px] h-[150px] object-cover rounded-lg"
+              />
+              <div class="w-[150px] mt-2 font-semibold">
+                {{ category.name }}
+              </div>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <div v-else class="md:w-3/4 w-full">
+        <h3 class="text-lg font-semibold py-3">
+          <template v-if="searchResults.term">
+            <template v-if="searchResults.results.length !== 0">
+              Showing all search results for
+              <span class="text-red-600">{{ searchResults.term }}</span>
+            </template>
+            <template v-else
+              >No results found for
+              <span class="text-red-600">{{ searchResults.term }}</span>
+            </template>
+          </template>
+          <template v-else> Recommended products </template>
+        </h3>
+        <div class="flex flex-wrap gap-4">
+          <div v-for="product in searchResults.results" :key="product.id">
+            <NuxtLink
+              :to="`/product/${product.id}`"
+              @click="closeModal"
+              class="flex flex-col items-center hover:text-pink-600 hover:scale-105 transition duration-500"
+            >
+              <img
+                :src="product.oneImg || product.images[0] || '/favicon.ico'"
+                :alt="product.name"
+                class="w-[150px] h-[150px] object-cover rounded-lg"
+              />
+              <div class="w-[150px] mt-2 font-semibold">
+                {{ capitalize(product.name) }}
+              </div>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
 .u-modal-content {
-    max-height: 100vh;
-    overflow-x: hidden;
+  max-height: 100vh;
+  overflow-x: hidden;
 }
 
 .hide-scrollbar {
-    overflow-y: scroll;
-    scrollbar-width: none;
+  overflow-y: scroll;
+  scrollbar-width: none;
 }
 
 .hide-scrollbar::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 </style>
