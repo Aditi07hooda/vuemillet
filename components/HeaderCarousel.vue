1ccb7782f42d2dfb1495a78bb2a731c3af888a60
Aditi <aditi07hooda@gmail.com>
2025-02-06 16:08:42 +0530

    header carousel

---
+++ components/HeaderCarousel.vue
@@ -0,0 +1,19 @@
+<script setup>
+const config = useRuntimeConfig();
+const baseURL = config.public.baseURL;
+const brandID = config.public.brandID;
+const sessionId = ref(null);
+
+if (typeof window !== "undefined") {
+  sessionId.value = localStorage.getItem("sessionId");
+}
+
+if (!sessionId.value) {
+  sessionId.value = await createSessionId(baseURL, brandID);
+}
+</script>
+<template>
+    <div>
+        <h1>Header</h1>
+    </div>
+</template>
