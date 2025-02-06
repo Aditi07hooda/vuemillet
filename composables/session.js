eda62fdc12b7b3de66b8a4cba7fdce31fa917e93
Aditi <aditi07hooda@gmail.com>
2025-02-06 16:10:38 +0530

    fetch brand info

--- composables/session.js
+++ composables/session.js
@@ -1,19 +1,33 @@
 export const createSessionId = async (baseURL, brandID) => {
-    try {
-        const response = await fetch(`${baseURL}/store/${brandID}/init`, {
-            method: "POST",
-        })
+  try {
+    const response = await fetch(`${baseURL}/store/${brandID}/init`, {
+      method: "POST",
+    });
 
-        const data = await response.json()
-        const sessionId = data.session
+    const data = await response.json();
+    const sessionId = data.session;
 
-        if (typeof window !== "undefined") {
-            localStorage.setItem("sessionId", sessionId)
-        }
-
-        return sessionId
-    } catch (error) {
-        console.error("Unable to create session id", error)
-        return null
+    if (typeof window !== "undefined") {
+      localStorage.setItem("sessionId", sessionId);
     }
-}
+
+    return sessionId;
+  } catch (error) {
+    console.error("Unable to create session id", error);
+    return null;
+  }
+};
+
+export const fetchBrandInfo = async (baseURL, brandID) => {
+  try {
+    const response = await fetch(`${baseURL}/store/${brandID}/init`, {
+      method: "POST",
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Unable to create session id", error);
+    return null;
+  }
+};
