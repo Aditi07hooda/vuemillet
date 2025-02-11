68f55c772321b9694580b25f39e294444550c642
Aditi <aditi07hooda@gmail.com>
2025-02-11 17:37:33 +0530

    only first collection product

--- components/ShopNavbarModal.vue
+++ components/ShopNavbarModal.vue
@@ -38,41 +38,30 @@ defineProps({
     </div>
     <div class="mt-10 col-span-4 row-span-4">
       <div class="flex gap-4 flex-wrap flex-col">
-        <div v-for="collection in products" :key="collection.id" class="w-full">
-          <NuxtLink
-            :to="`/collections/${collection.collectionDetail.id}`"
-            @click="close"
-            class="hover:text-pink-600 hover:scale-105 transition duration-500"
+        <div class="flex flex-row">
+          <div
+            v-for="p in products[0].products.slice(0, 5)"
+            :key="p.id"
+            class="w-fit mx-3"
           >
-            <h4 class="text-base font-semibold py-3">
-              {{ collection.collectionDetail.name }}
-            </h4>
-          </NuxtLink>
-          <div class="flex flex-row">
-            <div
-              v-for="p in collection.products.slice(0, 5)"
-              :key="p.id"
-              class="w-fit mx-3"
+            <NuxtLink
+              :to="`/product/${p.id}`"
+              @click="close"
+              class="flex flex-col hover:text-pink-600 hover:scale-105 transition duration-500"
             >
-              <NuxtLink
-                :to="`/product/${p.id}`"
-                @click="close"
-                class="flex flex-col hover:text-pink-600 hover:scale-105 transition duration-500"
-              >
-                <img
-                  :src="p.oneImg || p.images[0] || '/favicon.ico'"
-                  :alt="p.name"
-                  class="w-[150px] h-[150px] object-cover rounded-lg"
-                />
-                <div class="w-[150px] mt-2 font-normal text-sm">
-                  {{ p.name }}
-                </div>
-              </NuxtLink>
-            </div>
+              <img
+                :src="p.oneImg || p.images[0] || '/favicon.ico'"
+                :alt="p.name"
+                class="w-[150px] h-[150px] object-cover rounded-lg"
+              />
+              <div class="w-[150px] mt-2 font-normal text-sm">
+                {{ p.name }}
+              </div>
+            </NuxtLink>
           </div>
           <div
             class="flex flex-col gap-3 mt-4"
-            v-if="collection.products.length > 5"
+            v-if="products[0].products.length > 5"
           >
             <NuxtLink
               :to="`/collections/${collection.collectionDetail.id}`"
