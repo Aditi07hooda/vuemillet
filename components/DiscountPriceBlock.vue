a65be7257831ec2686656d3ed5bee6bd786cc11d
Aditi <aditi07hooda@gmail.com>
2025-02-20 20:17:42 +0530

    price block more prominent

--- components/DiscountPriceBlock.vue
+++ components/DiscountPriceBlock.vue
@@ -4,16 +4,25 @@ defineProps({
   offerPrice: { type: Number, required: true },
   style: { type: String },
 });
+
+const hasDiscount = (price, offerPrice) => price > offerPrice;
 </script>
 
 <template>
-  <template v-if="hasDiscount(price, offerPrice)">
-    <div class="flex gap-5 justify-center items-center">
-      <div class="text-rose-600">
-        {{ calculateDiscount(price, offerPrice) }} % off
-      </div>
-      <span class="line-through me-1 text-gray-600">₹ {{ price }}</span>
-    </div>
-  </template>
-  <span :style="style" class="text-green-600 text-xl font-bold">₹ {{ offerPrice }}</span>
+  <div class="flex items-center gap-3 justify-center">
+    <template v-if="hasDiscount(price, offerPrice)">
+      <span class="text-gray-500 text-2xl line-through">Rs. {{ price }}</span>
+      <span class="text-green-600 text-3xl font-bold"
+        >Rs. {{ offerPrice }}</span
+      >
+      <span class="text-rose-600 text-2xl font-semibold">
+        ({{ calculateDiscount(price, offerPrice) }}% off)
+      </span>
+    </template>
+    <template v-else>
+      <span class="text-green-600 text-3xl font-bold"
+        >Rs. {{ offerPrice }}</span
+      >
+    </template>
+  </div>
 </template>
