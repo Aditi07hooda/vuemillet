019af9b1b83a5a9018e4ffbb2dc1594f86729b2c
Aditi <aditi07hooda@gmail.com>
2025-02-26 22:46:00 +0530

    variant slection refractor

--- components/VariantSelection.vue
+++ components/VariantSelection.vue
@@ -6,7 +6,7 @@
     >
       <div
         v-for="option in product.variants"
-        :key="option.name"
+        :key="option?.name"
         @click="logOptionSize(option)"
         class="px-4 mb-2 p-2 options cursor-pointer border-2 border-black flex gap-3 rounded items-center"
         :class="selectedSize === option ? 'selected' : ''"
@@ -22,7 +22,7 @@
     >
       <div
         v-for="option in product.variants"
-        :key="option.name"
+        :key="option?.name"
         @click="logOptionSize(option)"
         class="rounded-full px-2 mx-2 mb-2 p-2 options cursor-pointer flex items-center gap-2"
         :class="selectedSize === option ? 'selected' : ''"
@@ -30,7 +30,7 @@
         <p
           class="rounded-full w-5 h-5 inline-block"
           :style="{
-            backgroundColor: variantColor[option.name] || 'gray',
+            backgroundColor: variantColor[option?.name] || 'gray',
           }"
         ></p>
         <span>{{ option.name }}</span>
@@ -66,7 +66,7 @@
     <div class="flex flex-wrap" v-else>
       <div
         v-for="option in product.variants"
-        :key="option.name"
+        :key="option?.name"
         @click="logOptionSize(option)"
         class="rounded-full px-2 mx-2 mb-2 p-2 options transition duration-500 cursor-pointer"
         :class="selectedSize === option ? 'selected' : ''"
@@ -77,18 +77,38 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    product: Object,
-    variantImage: Object,
-    variantColor: Object,
-    selectedSize: Object,
+<script setup>
+import { defineProps, defineEmits, onMounted } from "vue";
+
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true,
   },
-  methods: {
-    logOptionSize(option) {
-      this.$emit("update:selectedSize", option);
-    },
+  variantImage: {
+    type: Object,
+    default: () => ({}),
   },
+  variantColor: {
+    type: Object,
+    default: () => ({}),
+  },
+  selectedSize: {
+    type: Object,
+    default: () => null,
+  },
+});
+
+const emit = defineEmits(["update:selectedSize"]);
+
+const logOptionSize = (option) => {
+  emit("update:selectedSize", option);
 };
+
+// onMounted(() => {
+//   console.log("Product Data:", props.product);
+//   console.log("Variant Image Data:", props.variantImage);
+//   console.log("Variant Color Data:", props.variantColor);
+//   console.log("Selected Size:", props.selectedSize);
+// });
 </script>
