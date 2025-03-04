fb3d2a06890c4e17ce1b186fc6dc61740aa97772
Aditi <aditi07hooda@gmail.com>
2025-03-05 02:28:24 +0530

    font

--- plugins/webfontloader.client.ts
+++ plugins/webfontloader.client.ts
@@ -1,12 +1,11 @@
-import WebFont from "webfontloader"
-
 export default defineNuxtPlugin(() => {
-  WebFont.load({
-    google: {
-      families: ['Poppins:300,400,500,600,700'],
-    },
-    active() {
-      console.log('Google Fonts loaded');
-    },
-  });
-});
+    if (import.meta.server) return; // Prevents execution on the server
+  
+    import('webfontloader').then((WebFont) => {
+      WebFont.load({
+        google: {
+          families: ['Poppins:300,400,500,600&display=swap'],
+        },
+      });
+    });
+  });  
